9db46a2e7d7085231190284aa573a5e774418304
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2023-10-04 22:38:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    // <div className="parent">
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+    // </div>
+    // <div class="parent">
+    //   <div class="child">test</div>
+    // </div>
+  );
+}
